4b75aca76c758026662b44d807ecce3c8c91d1a1
rl-repo <richardpluu2@gmail.com>
2022-10-18 15:42:46 -0400

    More sections and icons

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function () { myFunction() };
 
 // // Get the header
 var header = document.getElementById("myHeader");
@@ -10,25 +10,44 @@ var sticky = header.offsetTop;
 
 // // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-   if (window.pageYOffset > sticky) {
-     header.classList.add("sticky");
-   } else {
-     header.classList.remove("sticky");
-   }
- } 
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
 
 
 
- function scrollFunction1() {
+function scrollFunction1() {
   let e = document.getElementById("about");
   e.scrollIntoView({
     block: 'start',
     behavior: 'smooth',
     inline: 'start'
+  });
+}
+
+function scrollFunction2() {
+  let e = document.getElementById("edu");
+  e.scrollIntoView({
+    block: 'start',
+    behavior: 'smooth',
+    inline: 'start'
+  });
+}
+
+function scrollFunction3() {
+  let e = document.getElementById("exp");
+  e.scrollIntoView({
+    block: 'start',
+    behavior: 'smooth',
+    inline: 'start'
    });
  }
- function scrollFunction2() {
-  let e = document.getElementById("edu");
+
+ function scrollFunction4() {
+  let e = document.getElementById("skills");
   e.scrollIntoView({
     block: 'start',
     behavior: 'smooth',
@@ -37,29 +56,40 @@ function myFunction() {
  }
 
 
- /*
- $(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+function scrollFunction5() {
+  let e = document.getElementById("projs");
+  e.scrollIntoView({
+    block: 'start',
+    behavior: 'smooth',
+    inline: 'start'
+   });
+ }
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
+/*
+$(document).ready(function(){
+ // Add smooth scrolling to all links
+ $("a").on('click', function(event) {
+
+   // Make sure this.hash has a value before overriding default behavior
+   if (this.hash !== "") {
+     // Prevent default anchor click behavior
+     event.preventDefault();
+
+     // Store hash
+     var hash = this.hash;
+
+     // Using jQuery's animate() method to add smooth page scroll
+     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+     $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 800, function(){
+  
+       // Add hash (#) to URL when done scrolling (default click behavior)
+       window.location.hash = hash;
+     });
+   } // End if
+ });
 });
 */
